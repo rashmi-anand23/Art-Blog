6ddc9cfc2c0aabc42ac4f84cda09b8981d45d46e
Rashmi <rashmianand252@gmail.com>
2024-07-17 00:22:00 +0530

    first commit

---
+++ dist/script.js
@@ -0,0 +1,20 @@
+const sr = ScrollReveal({
+    origin:"top",
+    distance:"60px",
+    duration:2000,
+    delay: 300
+  })
+
+  sr.reveal(".pencil__ani",{origin:"left"});
+  sr.reveal(".gouache__ani",{origin:"right"});
+  sr.reveal(".acrylic__ani",{origin:"left"});
+  sr.reveal(".watercolor__ani",{origin:"right"});
+  sr.reveal(".about__ani",{origin:"bottom"});
+  sr.reveal(".img1",{origin:"left"});
+  sr.reveal(".img2",{origin:"bottom"});
+  sr.reveal(".img3",{origin:"top"});
+  sr.reveal(".img4",{origin:"bottom"});
+  sr.reveal(".img5",{origin:"right"});
+  sr.reveal(".img6",{origin:"bottom"});
+  sr.reveal(".img7",{origin:"bottom"});
+  sr.reveal(".img8",{origin:"bottom"});
